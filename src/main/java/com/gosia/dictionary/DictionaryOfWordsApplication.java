78093352b7d791d6861e0dd2c4444abda0b41f8e
Małgorzata Latalska <malgorzata.latalska@clurgo.com>
2021-12-04 15:41:45 +0100

    feature: Project initialization on github

---
+++ src/main/java/com/gosia/dictionary/DictionaryOfWordsApplication.java
@@ -0,0 +1,13 @@
+package com.gosia.dictionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DictionaryOfWordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DictionaryOfWordsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gosia/dictionary/DictionaryOfWordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gosia.dictionary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DictionaryOfWordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
